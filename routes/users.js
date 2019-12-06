752ee253174aee395f964ba09d2f0b7c7695382f
LAPTOP-NKL7B1QI\emily <emily.potter@landmark.co.uk>
2019-12-06 15:14:34 +0000

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+const User = require('../models/user');
+
+// register
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            return res.json({success: false, msg: 'Failed to register user'});
+        } else {
+            return res.json({success: true, msg: 'User registered'});
+        }
+    })
+});
+
+
+// authenticate
+router.post('/authenticate', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.getUserByUsername(username, (err, user) => {
+        //if (err) throw err;
+        if (!user){
+            return res.json({success: false, msg: 'User not found'});
+        }
+
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            //if (err) throw err;
+            if (isMatch) {
+                const token = jwt.sign({data: user}, config.secret, {
+                    expiresIn: 604800 // 1 week
+                });
+
+                res.json({
+                    success: true,
+                    token: 'JWT ' + token,
+                    user: {
+                        id: user.id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                    }
+                });
+            } else {
+                return res.json({success: false, msg: 'Wrong password'});
+            }
+        });
+    });
+});
+
+// profile
+router.get('/test', (req, res, next) => { // redo auth from brad traversy vid here (can't remember how lol)
+    res.json({user: req.user});
+});
+
+router.get('/usertest', (req, res, next) => {
+    res.send('hey');
+});
+
+module.exports = router;
